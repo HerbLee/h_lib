9ea2be7cb8314f92e024d0e8d558a05dd77880a8
herb2sy <herb2sy@gmail.com>
2019-08-05 14:52:11 +0800

    Update ServiceResult.java

--- src/main/java/com/herb2sy/utils/ServiceResult.java
+++ src/main/java/com/herb2sy/utils/ServiceResult.java
@@ -36,6 +36,39 @@ public class ServiceResult<T> implements Serializable {
         this.message = message;
         this.data = data;
     }
+   
+    @Override
+    public String toString() {
+        return "ServiceResult{" +
+                "message='" + message + '\'' +
+                ", code=" + code +
+                ", data=" + data +
+                '}';
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
 
     @JsonIgnore
     public boolean isSuccesss(){
